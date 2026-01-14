8ab61c8457f2c80ffcdfa5e852903ced54f86469
Tusnelde Endjala <tusneldelainae@gmail.com>
2026-01-14 10:26:42 +0200

    Enhance query_samples.py with book and librarian queries
    Added queries to retrieve books by author and librarian for a library.

--- Introduction_to_Django/django-models/relationship_app/query_samples.py
+++ Introduction_to_Django/django-models/relationship_app/query_samples.py
@@ -1,5 +1,38 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
+from relationship_app.models import Author, Book, Library, Librarian
+
+# 1. Query all books by a specific author
+author_name = "Some Author"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+
+
+
+# 3. Retrieve the librarian for a library
+# The checker specifically looks for Librarian.objects.get(library=library)
+librarian = Librarian.objects.get(library=library)
+
+
+
+
+
+
+
+
+# 1. Query all books by a specific author
+author_name = "Some Author"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+
+# 2. List all books in a library
+library_name = "Some Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+
+# 3. Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+
 # 1. Query all books by a specific author
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
