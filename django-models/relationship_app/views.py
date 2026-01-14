ce7bab4e4292794564c497e17d95083f770624a6
Tusneld <tusneldelainae@gmail.com>
2026-01-14 02:29:31 +0200

    Task 2: Implement registration, login, and logout views

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Book
 from .models import Library
+
+# --- Task 1 Views ---
 
 # Function-based view to list all books
 def list_books(request):
@@ -13,3 +17,16 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+# --- Task 2: Authentication Views ---
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Automatically log the user in after registration
+            return redirect('list_books') # Redirect to the book list
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})                                                                               
